20f3011bdd6f70f1927369a561245e3567cfabd5
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2023-07-20 11:14:33 +0330

    Update profile-image.php

--- profile-image.php
+++ profile-image.php
@@ -1,6 +1,4 @@
 
-
-
 <style>
     .inputfile {
   /* visibility: hidden etc. wont work */
@@ -21,51 +19,79 @@
 }
 </style>
 
+<?php
+if (isset($_POST['update_avatar'])) {
+    if (file_exists($_FILES['avatar']['tmp_name'])) {
+        $path = 'uploads/' . time();
+        $path .= $_FILES['avatar']['name'];
+        if (!is_dir('uploads')) {
+            mkdir('uploads');
+        }
+        if (move_uploaded_file($_FILES['avatar']['tmp_name'], $path)) {
+            // Original uploaded image path
+            $originalImage = $path;
+            
+            // Desired width and height of the cropped image
+            $croppedSize = 200;
 
-<?php 
-if(isset($_POST['update_avatar'])){
+            // Load the original image
+            $image = imagecreatefromjpeg($originalImage);
+            $width = imagesx($image);
+            $height = imagesy($image);
 
-if(file_exists($_FILES['avatar']['tmp_name'])){
-    $path = 'uploads/'.time();
-    $path .= $_FILES['avatar']['name'];
-    if(!is_dir('uploads')){
-      mkdir('uploads');
+            // Calculate the coordinates for cropping
+            $cropX = 0;
+            $cropY = 0;
+            $cropSize = min($width, $height);
+
+            // Create a square cropped image
+            $croppedImage = imagecreatetruecolor($croppedSize, $croppedSize);
+            imagecopyresampled(
+                $croppedImage,
+                $image,
+                0,
+                0,
+                $cropX,
+                $cropY,
+                $croppedSize,
+                $croppedSize,
+                $cropSize,
+                $cropSize
+            );
+
+            // Save the cropped image
+            $croppedPath = 'uploads/cropped_' . time() . $_FILES['avatar']['name'];
+            imagejpeg($croppedImage, $croppedPath);
+
+            // Update the avatar field in the database
+            $id_profileme = $_POST['id_profile'];
+            @$rejister_form = $conn->prepare("UPDATE `users` SET `avatar` = :avatar WHERE id = $id_profileme");
+            @$rejister_form->execute(['avatar' => $croppedPath]);
+            
+            // Free up memory
+            imagedestroy($image);
+            imagedestroy($croppedImage);
+
+            echo "<meta http-equiv='refresh' content='0'>";
+        } else {
+            // File upload failed
+        }
+    } else {
+        // No file uploaded
     }
-  if(move_uploaded_file($_FILES['avatar']['tmp_name'], $path)){      
-
-     $iimage =  $path;
-  }else{      
-  }
-}else{
-}
-
-$id_profileme = $_POST['id_profile'];
-@$rejister_form = $conn->prepare("UPDATE `users` SET 
-`avatar` = :avatar 
-where id = $my_profile_id 
-");
-@$rejister_form->execute([
-    'avatar' => $iimage,
-]);
-echo "<meta http-equiv='refresh' content='0'>";
-
 }
 ?>
-
-
-
-
 
 <form action="" method="post" enctype="multipart/form-data">
 <center>
 
-<input name="id_profile" value="<?php echo $user_show->id ?>" style="display:none;" type="text" class="form-control" placeholder="">
+<input name="id_profile" value="<?php echo $my_profile_id ?>" style="display:none;" type="text" class="form-control" placeholder="">
 
 
 <label for="file">
 
 
-<img src="<?php echo $my_profile_avatar ?>"  width="200px" height="200px" style="border-radius:100%;">
+<img src="<?php echo $my_profile_avatar ?>"  width="50px" height="50px" class="img-circle">
 
 <input type="file" name="avatar" id="file" class="inputfile">
 <br>
