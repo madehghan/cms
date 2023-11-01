0c309cc9b33b27445b5f134ae16ec59246100218
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2023-11-02 00:24:07 +0330

    Create application-footer-fix.php

---
+++ application-footer-fix.php
@@ -0,0 +1,26 @@
+<style>
+.footer_home{
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+}
+.footer_home_items{
+    color:#212121;
+    font-size: 30px;
+}
+</style>
+
+<div class="footer_home fixed-bottom p-3 text-center">
+    <div class="row">
+        <div class="col">
+            <a href="profile.php" class="footer_home_items"><i class="bi bi-person-circle"></i></a>
+        </div>
+        <div class="col">
+            <a href="home.php" class="footer_home_items"><i class="bi bi-house-fill"></i></a>
+        </div>
+        <div class="col">
+            <a href="home.php" class="footer_home_items"><i class="bi bi-plus-circle"></i></a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
