36d1a5386384c01291d0da329952320d378aebcb
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2023-11-01 23:51:56 +0330

    Create application-login.php

---
+++ application-login.php
@@ -0,0 +1,27 @@
+
+<?php if(isset($my_profile_id)){ header('location:home.php');}?>
+<div class="container">
+<style>
+.login_form_bg{
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    background: #fff;
+    padding:15px;
+    margin: 10px;
+    margin-top:30vh;
+    border-radius: 8px;
+}
+</style>
+
+<div class="login_form_bg">
+<?php include 'modules/login.php' ?>
+<form action="" method="POST">
+<label class="p-3">جهت ورود یا عضویت شماره موبایل خود را وارد نمایید!</label>
+<input type="number" name="phone_number" class="form-control mt-2" placeholder="شماره موبایل">
+<button type="submit" class="btn btn-primary mt-2 w-100 btn-sm" name="get_sms">دریافت کد تایید</button>
+</form>
+</div>
+
+<div class="fixed-bottom text-center p-3">
+<a href="tel:<?php echo $setting_support_number ?>">تماس با پشتیبانی</a>
+</div>
+</div>
