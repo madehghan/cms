28339e225e25915d7263a7bed30ea2e2323b2287
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2023-11-05 16:49:09 +0330

    Create application-welcome.php

---
+++ application-welcome.php
@@ -0,0 +1,20 @@
+
+<div class="container">
+    <div class="logo_welcome text-center">
+        <img src="uploads/logo.png" class="w-50">
+        <div class="title_welcome"><?php echo $setting_site_name ?></div>
+        <div class="desc_welcome"><?php echo $setting_site_description ?></div>
+    </div>
+</div>
+
+<div class="fixed-bottom text-center p-3">
+    در حال بارگذاری اطلاعات...
+</div>
+
+
+<script type="text/javascript">
+        setTimeout(function () {
+            window.location.href = 'login.php'; // Replace with the URL you want to redirect to
+        }, 3000); // 3000 milliseconds = 3 seconds
+    </script>
+
