64de85de8b419df97cf7111b89094b20c8dc8f76
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2024-04-29 11:29:23 +0330

    Create wallet-show-dashboard.php

---
+++ wallet-show-dashboard.php
@@ -0,0 +1,5 @@
+<div class="bg-primary rounded p-3 text-center text-light">
+    <h6> <i class="bi bi-wallet2"></i> موجودی کیف پول</h6>
+    <hr>
+    <div class=""><?php echo number_format($my_profile_wallet) ?> تومان </div>
+</div>
