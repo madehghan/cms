c015099eb973f3c3dfcedb904e25e61fcbdaa7a2
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2024-04-29 12:26:02 +0330

    Update wallet-show-dashboard.php

--- wallet-show-dashboard.php
+++ wallet-show-dashboard.php
@@ -1,5 +1,21 @@
-<div class="bg-primary rounded p-3 text-center text-light">
+<div class="bg-primary rounded p-3 text-center text-light mb-3">
     <h6> <i class="bi bi-wallet2"></i> موجودی کیف پول</h6>
     <hr>
-    <div class=""><?php echo number_format($my_profile_wallet) ?> تومان </div>
+    <div class="">
+    
+    
+    <?php
+try {
+    // Assuming $conn is your PDO connection object
+    $stmt = $conn->prepare("SELECT SUM(price) AS total_price FROM payments WHERE user_id = :user_id");
+    $stmt->bindParam(':user_id', $my_profile_id);
+    $stmt->execute();
+    $total = $stmt->fetch(PDO::FETCH_ASSOC);
+    $total_price = $total['total_price'];
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
+<?php echo number_format($total_price) ?> 
+ تومان </div>
 </div>
