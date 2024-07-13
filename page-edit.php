4de97baec655b77754ef1fbbbdd9f435850f3ac8
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2024-07-13 19:52:04 +0330

    Update page-edit.php

--- page-edit.php
+++ page-edit.php
@@ -1,21 +1,28 @@
 
-<?php include 'modules/page-edit.php' ?>
+<?php
+$getid = $_GET['id'];
+try{
+$readdb = $conn->query("select * from pages where id=$getid");
+$read_db = $readdb->fetchAll(PDO::FETCH_OBJ);
+}catch(Exception $e){
+echo  $e->getMessage();
+}
+?>
+<?php foreach($read_db as $information) :?>
+
 
 <?php
-if(isset($_POST['update']) && !file_exists($_FILES['file']['tmp_name'])){
+if(isset($_POST['update'])){
 try{
-    
-$getid = $_POST['getid'];
-$title = $_POST['title'];
-$content = $_POST['content'];
 $updatedb = $conn->prepare("UPDATE `pages` SET 
 `title` = :title ,
 `content` = :content 
 WHERE id = $getid ");
 $updatedb->execute([
-'title' => $title,
-'content' => $content,
+'title' => $_POST['title'],
+'content' => $_POST['content'],
 ]);
+
 
 echo "<meta http-equiv='refresh' content='0'>";
 
@@ -23,26 +30,12 @@ echo "<meta http-equiv='refresh' content='0'>";
 echo  $e->getMessage();
 }
 }
-
 ?>
 
 
-
-<?php
-$get_id = $_GET['id'];
-try{
-$readdb = $conn->query("select * from pages where id=$get_id");
-$read_db = $readdb->fetchAll(PDO::FETCH_OBJ);
-}catch(Exception $e){
-echo  $e->getMessage();
-}
-?>
-<?php foreach($read_db as $information) :?>
-<form action="" method="POST"  enctype="multipart/form-data">
-<input value="<?php echo $information->id ?>" name="getid" style="display:none;">
-<input type="text" name="title" class="form-control mt-2 mb-2" value="<?php echo $information->title ?>" placeholder="تیتر صفحه">
-<textarea  class="form-control mt-2 mb-2" name="content"><?php echo $information->content ?></textarea>
-
+<form action="" method="POST">
+<input type="" name="title" class="form-control mt-2 mb-2" value="<?php echo $information->title ?>" placeholder="عنوان">
+<textarea  class="form-control mt-2" name="content" placeholder="توضیحات" style="height:200px;"><?php echo $information->content ?></textarea>
 <button type="submit" name="update" class="btn btn-primary mt-2 w-100 btn-sm">به روز رسانی</button>
 </form>
 <?php endforeach; ?>
