0122c6dd688a09fbe5b6f924aac78973e5fc565b
محمدعلی دهقان | Mohammad Ali Dehghan <92730818+madehghan@users.noreply.github.com>
2024-09-07 14:25:49 +0330

    Create pwa-new.php

---
+++ pwa-new.php
@@ -0,0 +1,31 @@
+manifest.json
+
+{
+  "name": "",
+  "short_name": "",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "logo.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "logo.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
+
+
+<link rel="manifest" href="/manifest.json">
+
+<!-- Ensure full-screen mode for iOS -->
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<link rel="apple-touch-icon" href="logo.png">
+
